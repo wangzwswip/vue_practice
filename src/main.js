0afb3f23e1e147ea996138f7a2e2c6321074204d
wangzwswip <wangzw410@gmial.com>
2020-06-05 16:20:51 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import less from 'less'
+//导入全局样式
+import './assets/css/global.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/element.js'
 
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.use(less)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
+      path: '/login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从那个路径跳转而来
+  // next 是一个函数， 表示放行
+
+  if (to.path === '/login') return next();
+  // 获取token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
