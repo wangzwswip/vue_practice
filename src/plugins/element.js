a1b7e770eb3067a60fd4f10cd8532dab204c4dfb
wangzwswip <wangzw410@gmial.com>
2020-06-07 12:12:29 +0800

    完成用户列表的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
-import {Button, Form, FormItem, Input, Message} from 'element-ui'
+import {Button, Form, FormItem, Input, Message, Container, Header, Aside, Main,
+Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card,
+Row, Col, TableColumn, Table, Switch, Tooltip, Pagination, Dialog,
+MessageBox} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Main)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 // 消息需要全局挂载
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
