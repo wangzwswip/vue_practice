527f448bf0cdc22a82fe418c061fad55ee056eca
wangzwswip <wangzw410@gmial.com>
2020-06-09 18:36:59 +0800

    最后提交

--- src/main.js
+++ src/main.js
@@ -10,14 +10,24 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './plugins/element.js'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入进度条包
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
-  console.log(config)
+  // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 必须在最后返回config
+  return config
+})
+// 在response拦截器隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
